d9935d02edd3364cc1651ebb935af9d598cde036
pikolala <pikolala@list.ru>
2024-01-09 15:49:57 +0300

    added sound

---
+++ levels/level2/terrain.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="terrain" tilewidth="36" tileheight="36" tilecount="256" columns="16">
+ <image source="../../second_level_materials/TX Tileset Ground.png" width="576" height="576"/>
+</tileset>
